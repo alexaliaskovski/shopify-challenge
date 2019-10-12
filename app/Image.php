f5ec8d0cdb8a40628b83aabc8f3fb3a21c59f591
alexaliaskovski <alexa.liaskovski@gmail.com>
2019-10-12 14:17:23 -0700

    Update image and product models.

--- app/Image.php
+++ app/Image.php
@@ -21,30 +21,41 @@ class Image extends Model
     protected $primaryKey = "product_id";
 
     /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'uuid';
+
+    /**
      * @var boolean
      */
-    public $incrementing = false;
+    public $incrementing = false; // randomly assigned uuid
 
     /**
-     * @var array
-     */
-    protected $fillable = [
-        'product_id',
-        'url',
-        'alt_text',
-    ];
-
-    /**
-     * @var array
-     */
-    protected $attributes = [
-        'product_id' => '',
-        'url' => '',
-        'alt_text' => '',
-    ];
-
-    /**
+     * Specifies timestamps are not being stored.
+     * 
      * @var boolean
      */
     public $timestamps = false;
+
+    /**
+     * Specifies variables which are not mass-assignable.
+     * 
+     * @var array
+     */
+    protected $guarded = [
+        'product_id',
+    ];
+
+    /**
+     * The model's default values for attributes.
+     * 
+     * @var array
+     */
+    // protected $attributes = [
+    //     'product_id' => '',
+    //     'url' => '',
+    //     'alt_text' => '',
+    // ];
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,55 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * The name of the table associated to this model.
+     * 
+     * @var string
+     */
+    protected $table = 'products';
+
+    /**
+     * The primary key associated with the table.
+     * 
+     * @var string
+     */
+    protected $primaryKey = "product_id";
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'uuid';
+
+    /**
+     * @var boolean
+     */
+    public $incrementing = false; // randomly assigned uuid
+
+    /**
+     * Specifies timestamps are not being stored.
+     * 
+     * @var boolean
+     */
+    public $timestamps = false;
+
+    /**
+     * Specifies variables which are not mass-assignable.
+     * 
+     * @var array
+     */
+    protected $guarded = [
+        'product_id',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $attributes = [
+        'price' => 0.00,
+        'discount' => 0.00,
+        'quantity_stocked' => 0,
+        'quantity_sold' => 0,
+    ];
 }
